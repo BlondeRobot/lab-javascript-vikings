2ab6c5787a9a9b06ae79043417658d0ee1d24ec4
Alicia Kmak <alicia.kmak@gmail.com>
2021-03-01 13:30:19 +0100

    add completed bonuses. Everything works ok

--- src/viking.js
+++ src/viking.js
@@ -58,15 +58,33 @@ class War {
     }
 
  vikingAttack() {
+    
+
+    let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let randomViking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
+
+    let damage = randomSaxon.receiveDamage(randomViking.strength);
+
+    if (randomSaxon.health <= 0) {this.saxonArmy.pop()};
+    return damage
+
 
  }
 
  saxonAttack() {
+    let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let randomViking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
 
+    let damage = randomViking.receiveDamage(randomSaxon.strength);
+
+    if (randomViking.health <= 0) {this.vikingArmy.pop()};
+    return damage
  }
 
  showStatus() {
-
+   if (this.saxonArmy.length === 0) {return 'Vikings have won the war of the century!'}
+   else if (this.vikingArmy.length === 0) {return 'Saxons have fought for their lives and survived another day...'}
+   else if (this.saxonArmy.length >= 1 && this.vikingArmy.length >= 1) {return 'Vikings and Saxons are still in the thick of battle.'}
  }
 
 }
